7f5ddb9aa2d9bc51d3e24067684ec8f8ab77c8ab
Chris Soueidan <chris.soueidan@gmail.com>
2018-04-02 08:54:16 -0400

    added parallax and finished footer

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,7 +1,31 @@
 $(function () {
-  'use strict'
+    'use strict'
 
-  $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open')
-  })
+    $('[data-toggle="offcanvas"]').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
 })
+
+// parallax
+
+//parallax background jumbotron
+var $ = jQuery.noConflict()
+$(window).scroll(function () {
+    var scroll = $(this).scrollTop()
+    $(".section-intro").css({
+        "background-position": "right " + scroll / 3 + "px"
+    })
+})
+
+//parallax all elements
+$(document).ready(function () {
+    $(window).bind('scroll', function (e) {
+        parallax();
+    });
+});
+
+function parallax() {
+    var scrollPosition = $(window).scrollTop();
+    $('.jumbotron-graphic').css('top', (0 + (scrollPosition * 0.5)) + 'px');
+    $('.what-we-graphic').css('top', (0 - (scrollPosition * 0.1)) + 'px');
+}
